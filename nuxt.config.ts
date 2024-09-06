8a0d52e99f4165289d075a4fa0966c1c2a586402
caksaji <caksaaji@gmail.com>
2024-09-06 15:09:53 +0700

    [UPDATE] change logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+    },
     layoutTransition: { name: 'layout', mode: 'out-in' },
     pageTransition: { name: 'page', mode: 'out-in' }
   },
