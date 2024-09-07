266f358d2580c6fd1e9fe029ac8856c645b0da99
caksaji <caksaaji@gmail.com>
2024-09-07 18:11:20 +0700

    [UPDATE] index : simulate login

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="h-full pt-4" :style="{ paddingBottom: chatAreaPadding }">
+    <div class="h-full pt-4 transform duration-300" :style="{ paddingBottom: chatAreaPadding }">
       <div class="container max-w-screen-md" style="line-height: 1.25;">
         <div
           v-for="(m, index) in message"
@@ -31,7 +31,7 @@
         </div>
       </div>
     </div>
-    <div class="fixed bottom-0 left-0 w-screen bg-gray-100 dark:bg-gray-900">
+    <div class="fixed bottom-0 left-0 w-screen bg-gray-100 transform duration-300 dark:bg-gray-900" :class="{ 'translate-y-full opacity-0': disableChat }">
       <div class="container max-w-screen-md py-4">
         <div class="flex items-end justify-between w-full space-x-2 p-1 rounded-[1.7rem] bg-gray-300 dark:bg-gray-700">
           <div class="w-full py-1 pl-2">
@@ -40,6 +40,7 @@
               ref="input"
               rows="1"
               placeholder="Ketik sesuatu disini"
+              :disabled="disableChat"
               class="max-h-40 w-full outline-0 resize-none bg-transparent placeholder:text-gray-400"
               @keydown="inputKeydown"
               @input="setInputHeight"
@@ -70,6 +71,8 @@ const input = ref(null)
 const chatAreaPadding = ref()
 const cmd = ref()
 const message = ref([])
+const inputLogin = ref({ un: null, pw: null })
+const disableChat = ref(true)
 
 onMounted(() => {
   setInputHeight()
@@ -77,18 +80,11 @@ onMounted(() => {
     loadChat()
   }
   else {
-    message.value.push({
-      by: 'friend',
-      message: 'Halo👋 gimana kabarnya? Yuk kenalan dulu...',
-      at: `${new Date().getHours().toString()}:${new Date().getMinutes().toString()}`
-    })
+    addBubble({ by: 'friend', message: 'Halo👋 apa kabar? Yuk kenalan...', at: 'now' })
     setTimeout(() => {
-      message.value.push({
-        by: 'friend',
-        message: 'Masukkan username kamu',
-        at: `${new Date().getHours().toString()}:${new Date().getMinutes().toString()}`
-      })
-    }, 3000)
+      addBubble({ by: 'friend', message: 'Masukkan username kamu', at: 'now' })
+      disableChat.value = false
+    }, 1000)
   }
 })
 
@@ -96,7 +92,7 @@ const setInputHeight = () => {
   const inp = input.value
   inp.style.height = 'auto'
   inp.style.height = `${input.value.scrollHeight}px`
-  chatAreaPadding.value = `calc((.25rem * 2) + (.25rem * 2) + 3rem + ${inp.style.height})`
+  chatAreaPadding.value = !disableChat.value ? `calc((.25rem * 2) + (.25rem * 2) + 3rem + ${inp.style.height})` : '1rem'
 }
 const loadChat = () => {
   message.value = [
@@ -123,14 +119,43 @@ const inputKeydown = (e) => {
     submitInput()
   }
 }
-const submitInput = () => {
+const addBubble = (chat) => {
   message.value.push({
-    by: 'me',
-    message: cmd.value,
-    at: `${new Date().getHours().toString()}:${new Date().getMinutes().toString()}`
+    by: chat.by,
+    message: chat.message,
+    at: chat.at === 'now' ? `${new Date().getHours().toString()}:${new Date().getMinutes().toString()}` : chat.at
   })
+}
+const submitInput = () => {
+  if (!disableChat.value) {
+    if (userCookie.value) {
+      addBubble()
+    }
+    else {
+      if (!inputLogin.value.un) {
+        addBubble({ by: 'me', message: cmd.value, at: 'now' })
+        inputLogin.value.un = cmd.value
+        setTimeout(() => addBubble({ by: 'friend', message: 'Sekarang masukkan kata sandi', at: 'now' }), 1000)
+      }
+      else {
+        addBubble({ by: 'me', message: '•••••••••', at: 'now' })
+        inputLogin.value.pw = cmd.value
+        login()
+      }
+    }
+    resetInput()
+  }
+}
+const resetInput = () => {
   setTimeout(() => cmd.value = null, 500)
   setTimeout(() => setInputHeight(), 700)
+}
+const login = () => {
+  disableChat.value = true
+  setTimeout(() => addBubble({ by: 'friend', message: 'Tunggu sebentar...', at: 'now' }), 1000)
+  alert('Login')
+  addBubble({ by: 'friend', message: 'Halo username', at: 'now' })
+  disableChat.value = false
 }
 </script>
 
